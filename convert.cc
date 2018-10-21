9e4a9978b4767ffef2c36cf32b978fdb6902319f
bert hubert <bert.hubert@netherlabs.nl>
2018-10-21 23:13:56 +0200

    add RFCs, reclassify some of them, update conversion tool

--- convert.cc
+++ convert.cc
@@ -2,71 +2,204 @@
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/foreach.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/key_extractors.hpp>
+#include <unordered_map>
+using namespace ::boost::multi_index;
 #include <string>
 #include <set>
 #include <exception>
 #include <iostream>
+#include <fstream>
+#include <unordered_set>
+#include <map>
 namespace pt = boost::property_tree;
 
-using std::cout;
-using std::endl;
+using namespace std;
+
+struct RFCEntry
+{
+  std::string name;
+  time_t creation;
+  set<std::string> obsoletes;
+  set<std::string> obsoletedBy;
+  string title;  
+
+  string currentStatus;
+  int pages{0};
+};
+
+struct NameTag{};
+struct TimeTag{};
+
+typedef multi_index_container<
+  RFCEntry,
+  indexed_by<
+    ordered_non_unique<
+      tag<TimeTag>,
+      member<RFCEntry, time_t, &RFCEntry::creation> 
+      >,
+  ordered_unique<
+    tag<NameTag>,
+    member<RFCEntry, std::string, &RFCEntry::name>
+    >
+    >
+  > index_t;
+index_t g_index;
+
 
 int main(int argc, char** argv)
 {
-    // Create empty property tree object
-    pt::ptree tree;
+  ifstream dnsrfcstxt("dns-rfcs.txt");
+  std::unordered_set<string> dnsrfcs;
+  std::string line;
+  while(getline(dnsrfcstxt, line)) {
+    boost::trim(line);
+    boost::to_upper(line);
+    dnsrfcs.insert(line);
+  }
+  
+  // Create empty property tree object
+  pt::ptree tree;
+  
+  // Parse the XML into the property tree.
+  pt::read_xml(argv[1], tree);
+  
+  auto idx=tree.get_child("rfc-index");
+  int count=0;
 
-    // Parse the XML into the property tree.
-    pt::read_xml(argv[1], tree);
+  for(const auto& v : idx) {
+    if(v.first=="rfc-entry") {
+      RFCEntry re;
+      re.name = v.second.get_child("doc-id").data();
 
+      if(!dnsrfcs.count(re.name))
+        continue;
+      
+      re.title = v.second.get_child("title").data();
+      re.currentStatus=v.second.get_child("current-status").data();
 
-    auto idx=tree.get_child("rfc-index");
-    int count=0;
-    std::string title;
-    cout << "{";
-    for(const auto& v : idx) {
-      if(v.first=="rfc-entry") {
-        if(count)
-          cout<<",\n";
-        count++;
-        //        std::cout<<v.second.get_child("doc-id").data()<<" \""<<v.second.get_child("date.month").data()<<" " <<v.second.get_child("date.year").data()<<"\"\n";
-        cout << '"' << v.second.get_child("doc-id").data() << "\": {";
-        cout << "\"docID\": \"" << v.second.get_child("doc-id").data() << "\",";
-        title = v.second.get_child("title").data();
-        boost::replace_all(title, "\\", "\\\\");
-        boost::replace_all(title, "\"", "\\\"");
-        cout << "\"title\": \"" << title << "\", ";
-        cout << "\"currentStatus\": \"" << v.second.get_child("current-status").data() << "\",";
-        if(v.second.get_child("format").count("page-count"))
-          cout << "\"pages\": " << v.second.get_child("format.page-count").data() << ",";
-        else
-          cout << "\"pages\": 0,";
-        if(v.second.count("obsoleted-by"))
-          cout << "\"obsoleted\": 1"<<",";
-        else
-          cout << "\"obsoleted\": 0"<<",";
-        cout << "\"draft\": 0";
-        cout << "}";
+      count++;
+
+      if(v.second.count("obsoleted-by")) {
+        for(const auto obs : v.second.get_child("obsoleted-by")) {
+          re.obsoletedBy.insert(obs.second.data());
+          if(!dnsrfcs.count(obs.second.data())) {
+            cerr<<re.name<<" " <<re.title<<" is obsoleted by "<< obs.second.data()<< " which itself is not a DNS RFC!" <<endl;
+          }
+        }
+      }
+      if(v.second.count("obsoletes")) {
+        for(const auto obs : v.second.get_child("obsoletes")) {
+          re.obsoletes.insert(obs.second.data());
+          if(!dnsrfcs.count(obs.second.data())) {
+            cerr<<re.name<<" " <<re.title<<" obsoletes"<< obs.second.data()<< " which itself is not a DNS RFC!" <<endl;
+          }
+        }
       }
 
+      if(v.second.count("updated-by")) {
+        for(const auto obs : v.second.get_child("updated-by")) {
+          re.obsoletedBy.insert(obs.second.data());
+          if(!dnsrfcs.count(obs.second.data())) {
+            cerr<<re.name<<" " <<re.title<<" is updated by "<< obs.second.data()<< " which itself is not a DNS RFC!" <<endl;
+          }
+        }
+      }
+
+
+      if(v.second.get_child("format").count("page-count")) {
+        re.pages = atoi(v.second.get_child("format.page-count").data().c_str());
+
+        string date = v.second.get_child("date.month").data();
+        date+=" ";
+        date +=v.second.get_child("date.year").data();
+        
+        struct tm tm;
+        memset(&tm, 0, sizeof(tm));
+        tm.tm_mday = 1;
+        strptime(date.c_str(), "%B %Y", &tm);
+        re.creation = mktime(&tm);
+      }
+      g_index.insert(re);
     }
-    cout << "}"<<endl;
-    #if 0
+  }
+  cout<<"Have "<<g_index.size()<<" DNS RFCs"<<endl;
+  std::map<string, int> statusPageCount;
+  statusPageCount["OBSOLETED"];
+  for(const auto& re : g_index) {
+    statusPageCount[re.currentStatus];
+    cout << re.name << endl;
+  }
+  ofstream plot("plot");
+  plot << "# TIME";
+  for(const auto& spc : statusPageCount)
+    plot << '\t' << spc.first;
+  plot << "\n";
+
+  set<string> dedup;
+  for(const auto& re : g_index) {
+    plot << re.creation;
+    statusPageCount[re.currentStatus] += re.pages;
+
+    auto& idx = g_index.get<NameTag>();
+    for(const auto& obsoletes : re.obsoletes) {
+      if(auto iter = idx.find(obsoletes); iter != idx.end()) {
+        if(!dedup.count(iter->name)) {
+          statusPageCount[iter->currentStatus] -= iter->pages;
+          statusPageCount["OBSOLETED"] += iter->pages;
+          dedup.insert(iter->name);
+        }
+        else {
+          cout << re.name <<" tried to re-obsolete "<< iter-> name << endl;
+        }
+      }
+      else {
+        cerr<<"Could not find the thing it obsoletes: "<< obsoletes << endl;
+      }
+    }
     
-    // Use the throwing version of get to find the debug filename.
-    // If the path cannot be resolved, an exception is thrown.
-    m_file = tree.get<std::string>("debug.filename");
+    for(const auto& spc : statusPageCount)
+      plot << '\t' << spc.second;
 
-    // Use the default-value version of get to find the debug level.
-    // Note that the default value is used to deduce the target type.
-    m_level = tree.get("debug.level", 0);
-
-    // Use get_child to find the node containing the modules, and iterate over
-    // its children. If the path cannot be resolved, get_child throws.
-    // A C++11 for-range loop would also work.
-    BOOST_FOREACH(pt::ptree::value_type &v, tree.get_child("debug.modules")) {
-        // The data function is used to access the data stored in a node.
-        m_modules.insert(v.second.data());
+    plot <<" # "<< re.name;
+    for(const auto& obsoletes : re.obsoletes) {
+      plot << "\t-"<<obsoletes;
     }
-#endif
+    
+    plot << "\n";
+  }
+#if 0
+
+  int bcppages{0}, infopages{0}, exppages{0}, stdpages{0}, histopages{0}, otherpages{0}, obsopages{0};
+
+    cout << "{";
+        if(count)
+        cout<<",\n";
+
+
+      cout << '"' << re.name << "\": {";
+      cout << "\"docID\": \"" << re.name << "\",";
+
+      boost::replace_all(re.title, "\\", "\\\\");
+      boost::replace_all(re.title, "\"", "\\\"");
+      cout << "\"title\": \"" << re.title << "\", ";
+      cout << "\"currentStatus\": \"" << re.currentStatus << "\",";
+      cout << "\"pages\": " << re.pages << ",";
+      if(!re.obsoletedBy.empty()) {
+        cout << "\"obsoleted\": 1"<<",";
+      }
+      else
+        cout << "\"obsoleted\": 0"<<",";
+      else
+        cout << "\"pages\": 0,";
+
+      cout << "\"draft\": 0";
+      cout << "}";
+          
+          plot << mktime(&tm) << "\t" <<dnscount << "\t" << stdpages << "\t" << infopages << "\t" << exppages<<"\t" << histopages << "\t" << bcppages << "\t" << otherpages << "\t" << obsopages << endl;
+  
+  cout << "}"<<endl;
+  #endif
 }
